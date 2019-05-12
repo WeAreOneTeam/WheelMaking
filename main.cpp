b8adffbb6b278c003910d671aefe5afffcb0f6cc
housirzero <housirzero@gmail.com>
2019-05-12 22:28:32 +0800

    we are one team

---
+++ main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  WheelMaking
+//
+//  Created by housir on 2019/5/12.
+//  Copyright © 2019年 WeAreOneTeam. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
